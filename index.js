884ec13837725155683dbeee5ece5a8f10b8d6b4
Ben Kneale <bpkneale@gmail.com>
2021-02-07 08:05:10 +0800

    More logic to cart

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-const sgMail = require('@sendgrid/mail');
+let sgMail = require('@sendgrid/mail');
 
 // constants
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const renderEmail = (requestBody) => {
-    return JSON.stringify(requestBody, null, 2);
+const FromAddress = 'no-reply@cosycuppies.com.au';
+const ToCosyAddress = process.env.COSY_CUPPIES_ADDR;
+
+exports.mockSgMail = (mock) => {
+    sgMail = mock;
+}
+
+const renderEmail = (enquiry) => {
+    return JSON.stringify(enquiry, null, 2);
+}
+
+const renderEnquirerReply = (enquiry) => {
+    return JSON.stringify({
+        header: "Thankyou for your cart enquiry",
+        title: "Your cart enquiry has been submitted",
+        misc: "We will be in contact to confirm your order."
+    }, null, 2)
 }
 
 const response = (code, jsonBody) => {
@@ -17,23 +32,51 @@ const response = (code, jsonBody) => {
 // lambda function call
 exports.handler = async function(event) {
 
-    if(event.httpMethod === "POST") {
-        const msg = {
-            to: 'stephanie@cosycuppies.com.au',
-            from: 'api@cosycuppies.com.au',
-            subject: 'Cart submitted',
-            html: renderEmail(JSON.parse(event.body))
+    if(!(event.httpMethod === undefined || event.httpMethod === "POST")) {
+        return response(405, {success: false, status: "Unexpected method", extra: event.httpMethod})
+    }
+
+    let parsed = null;
+    try {
+        parsed = JSON.parse(event.body);
+    }
+    catch(err) {
+        return response(400, {success: false, status: "Unable to parse request into JSON", extra: err})
+    }
+
+    if(parsed && parsed.enquiry && typeof(parsed.enquiry.email) === "string") {
+
+        const enquirer = parsed.enquiry.email;
+
+        const toCosy = {
+            to: ToCosyAddress,
+            from: FromAddress,
+            subject: `Cart submitted from ${enquirer}`,
+            html: renderEmail(parsed.enquiry)
         };
 
+        const toEnquirer = {
+            to: enquirer,
+            from: FromAddress,
+            subject: `Cart enquiry for Cosy Cuppies`,
+            html: renderEnquirerReply(parsed.enquiry)
+        }
+
         try {
-            await sgMail.send(msg);
+            await sgMail.send(toEnquirer);
+        } catch(err) {
+            return response(500, {success: false, status: "Failed to submit cart", extra: err});
+        }
+    
+        try {
+            await sgMail.send(toCosy);
             return response(200, {success: true, status: "Sent successfully"});
         }
         catch (err) {
             return response(500, {success: false, status: "Failed to submit cart", extra: err});
         }
 
+    } else {
+        return response(400, "Unable to parse enquiry or find enquirer email address")
     }
-
-    return response(405, {success: false, status: "Failed to submit cart", extra: `Method ${event.httpMethod} not supported`});
 };
---
+++ index.test.js
@@ -0,0 +1,121 @@
+const { mockSgMail, handler } = require("./index");
+const { expect } = require("chai");
+
+class MockMail {
+    constructor(throwError) {
+        this.throwError = throwError;
+        this.messages = [];
+    }
+
+    send(message) {
+        this.messages.push(message);
+        if(this.throwError) {
+            return Promise.reject(this.throwError)
+        } else {
+            return Promise.resolve(true);
+        }
+    }
+}
+
+describe("Lambda tests", () => {
+
+    it("Should handle well formed packet", async () => {
+
+        const mock = new MockMail();
+
+        mockSgMail(mock);
+
+        const event = {
+            httpMethod: "POST",
+            body: JSON.stringify({
+                enquiry: {
+                    email: "test@example.com",
+                    cart: [
+                        {
+                            "cuppie": 5
+                        },
+                        {
+                            "duppie": 1
+                        }
+                    ]
+                }
+            })
+        }
+
+        const resp = await handler(event)
+
+        console.info({resp})
+
+        expect(resp.statusCode).to.eq(200);
+
+        const msgs = mock.messages;
+        expect(msgs.length).to.eq(2);
+
+    })
+
+    it("HTTP method should be optional", async () => {
+
+        const mock = new MockMail();
+
+        mockSgMail(mock);
+
+        const event = {
+            body: JSON.stringify({
+                enquiry: {
+                    email: "test@example.com",
+                    cart: [
+                        {
+                            "cuppie": 5
+                        },
+                        {
+                            "duppie": 1
+                        }
+                    ]
+                }
+            })
+        }
+
+        const resp = await handler(event)
+
+        console.info({resp})
+
+        expect(resp.statusCode).to.eq(200);
+
+        const msgs = mock.messages;
+        expect(msgs.length).to.eq(2);
+
+    })
+
+    it("Should require email address", async () => {
+
+        const mock = new MockMail();
+
+        mockSgMail(mock);
+
+        const event = {
+            httpMethod: "POST",
+            body: JSON.stringify({
+                enquiry: {
+                    cart: [
+                        {
+                            "cuppie": 5
+                        },
+                        {
+                            "duppie": 1
+                        }
+                    ]
+                }
+            })
+        }
+
+        const resp = await handler(event)
+
+        console.info({resp})
+
+        expect(resp.statusCode).to.eq(400);
+
+        const msgs = mock.messages;
+        expect(msgs.length).to.eq(0);
+
+    })
+})
